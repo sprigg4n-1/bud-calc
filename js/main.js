d9bad6efbb775d79b6ecb1596ca688897df02f12
Roman <sprigg4n.me@gmail.com>
2022-11-07 10:17:52 +0200

    Add function in btn

--- js/main.js
+++ js/main.js
@@ -2,9 +2,12 @@ const mainCalcBtn = document.querySelector('#calc-main');
 const homeBody = document.querySelector('.home');
 const backBtnMainCalc = document.querySelector('#backMainCalc');
 const mainCalc = document.querySelector('.calc-main');
+const createCalcBtn = document.querySelector('#calc-create');
+const createBody = document.querySelector('.create');
 
 const homeBtn = document.querySelectorAll('#home');
-const mainCalcNav = document.querySelector('#mainCalcNav')
+const mainCalcNav = document.querySelector('#mainCalcNav');
+const createCalcNav = document.querySelector('#createCalcNav');
 
 const inputMainCalc = document.querySelector('.calc-main__input');
 const calcMainTitle = document.querySelector('.calc-main__title');
@@ -27,14 +30,27 @@ mainCalcBtn.addEventListener('click', () => {
 });
 backBtnMainCalc.addEventListener('click', () => {
     mooveLeftBack();
+    homeBody.classList.remove('moove-right');
 });
 homeBtn.forEach(item => {
     item.addEventListener('click', () => {
         mooveLeftBack();
+        createBody.classList.add('moove-left');
+        homeBody.classList.remove('moove-right');
     })
 });
 mainCalcNav.addEventListener('click', () => {
     mooveLeft();
+    createBody.classList.add('moove-left');
+});
+createCalcBtn.addEventListener('click', () => {
+    createBody.classList.remove('moove-left');
+    homeBody.classList.add('moove-right');
+});
+createCalcNav.addEventListener('click', () => {
+    createBody.classList.remove('moove-left');
+    homeBody.classList.add('moove-right');
+    mainCalc.classList.add('moove-right');
 });
 
 // main calc function
